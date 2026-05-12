136908b630c91927f0a887bd4750db12ef8ec5c6
kazuo <kinniku.kin.gin@gmail.com>
2026-05-13 07:12:51 +0900

    Revert ad-related changes (App ID / banner unit ID / delegate)

--- GAtsume/MEMO.swift
+++ GAtsume/MEMO.swift
@@ -5,12 +5,7 @@
 //  Created by ISHIBASHI KAZUHIRO on 2026/05/09.
 
 
-// --- AdMob (GAtsume / 本番) ---
-// App ID:      ca-app-pub-2165259899292420~9595753171
-// Banner Unit: ca-app-pub-2165259899292420/1280930298
-//
-// --- 旧 (G集め / 使用停止) ---
-// App ID:      ca-app-pub-2165259899292420~6743804686
-// Banner Unit: ca-app-pub-2165259899292420/6791906149
+//ca-app-pub-2165259899292420~6743804686
+// ca-app-pub-2165259899292420/6791906149
 
 
